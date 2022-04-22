e44e010b16b13851d85d6ffe79052a8c251c605c
bewflast <68069182+bewflast@users.noreply.github.com>
2022-04-23 02:08:31 +0300

    Добавлены подмодули (DS RPC, CURL); Перенесены .h

---
+++ src/DRPC.h
@@ -0,0 +1,24 @@
+#ifndef DRPC_H
+#define DRPC_H
+
+# include <discord_register.h>
+# include <discord_rpc.h>
+# include "CURLClient.h"
+# include <iostream>
+# include <ctime>
+
+class DRPC
+{
+	private:
+		DiscordEventHandlers *handler;
+		DiscordRichPresence *presence;
+		const char* cid;
+
+	public:
+		DRPC(const char* cID);
+		void init();
+		void update(t_audioInfo info);
+
+};
+
+#endif 
