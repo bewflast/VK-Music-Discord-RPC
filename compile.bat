6f771faa50828ec1bfda25e8b085b465bdbf03d3
bewflast <68069182+bewflast@users.noreply.github.com>
2022-04-23 02:16:16 +0300

    правки в batch файле

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
-cd curl-master/winbuild
+cd curl/winbuild
 nmake /f Makefile.vc mode=static
 
-cd ../../discord-rpc-master
+cd ../../discord-rpc
 cmake -G "Visual Studio 16 2019" -A Win32 -S . -B "build32"
 cmake --build build32 --config Release
 cd ../
